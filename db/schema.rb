8bae3f89c45ea3a38691b36c595258f5fdefc38c
Rustin Player <rjplayer@outlook.com>
2018-04-10 14:11:32 -0600

    Changed Image column to be text datatype

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405212518) do
+ActiveRecord::Schema.define(version: 20180410200906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "image"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
   end
 
 end
