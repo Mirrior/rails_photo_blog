07a6b2de3a2b80184c867461b1aa32eb65ba9856
Rustin Player <rjplayer@outlook.com>
2018-04-11 10:31:11 -0600

    Added some image elements

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,9 +1,14 @@
 class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
   storage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process resize_to_fit:[200,200]
   end
 
   def extension_whitelist
